7c3288a7d9ec1d83b2f042add7d0495d74bf10e7
shougal <xdq9qa@virginia.edu>
2025-07-19 16:14:45 +0300

    feat: scaffold full-stack app with React frontend and Express backend

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config(); //loads env variables
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors()); // allows cross-origin requests
+app.use(express.json()); // parses incoming JSON request bodies
+
+app.get("/", (req, res) => {
+  res.send("Backend is working!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
