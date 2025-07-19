9564a5c02c31b16a821a814a3926f74d75d4ce42
shougal <xdq9qa@virginia.edu>
2025-07-19 17:35:36 +0300

    chore: add ESLint and Prettier setup for frontend and backend

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,29 +1,23 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
 import prettier from "eslint-config-prettier";
+
 export default tseslint.config([
   {
-    files: ["src/**/*.{ts,tsx}"],
+    files: ["src/**/*.ts"],
     languageOptions: {
       ecmaVersion: 2022,
-      globals: globals.browser,
+      sourceType: "module",
+      globals: globals.node,
     },
     plugins: {
       "@typescript-eslint": tseslint.plugin,
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
     },
     rules: {
       "@typescript-eslint/no-unused-vars": "error",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
+      "@typescript-eslint/explicit-function-return-type": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
     },
     extends: [js.configs.recommended, tseslint.configs.recommended, prettier],
   },
