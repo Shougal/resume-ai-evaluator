6c2d6a4dbfc2d2067214b8d6fed331fe9b785f70
shougal <xdq9qa@virginia.edu>
2025-07-21 11:10:12 +0300

    chore(theme): setup global theme sizings

---
+++ frontend/src/theme/layout.ts
@@ -0,0 +1,39 @@
+export const paddingStyle = {
+  xs: "8px",
+  sm: "12px",
+  md: "16px",
+  lg: "20px",
+  xlg: "24px",
+};
+
+export const fontSizeStyle = {
+  xs: "12px",
+  sm: "14px",
+  md: "16px",
+  lg: "18px",
+  xlg: "20px",
+};
+
+export const borderRadiusStyle = {
+  xs: "5px",
+  sm: "9px",
+  md: "13px",
+  lg: "17px",
+  xlg: "21px",
+};
+
+export const height = {
+  buttonHeight: "42px", //TODO: check if responsive
+};
+
+export const gapStyle = {
+  xs: "5px",
+  sm: "10px",
+  md: "20px",
+  lg: "30px",
+  xlg: "40px",
+};
+
+export const buttonBackground = {
+  primary: "linear-gradient(0.6turn, #763AF5,40%,  #A604F2 )",
+};
---
+++ frontend/src/theme/types.ts
@@ -0,0 +1,15 @@
+import {
+  paddingStyle,
+  fontSizeStyle,
+  borderRadiusStyle,
+  height,
+  gapStyle,
+} from "./layout";
+
+export type PaddingStyle = keyof typeof paddingStyle;
+export type FontSizeStyle = keyof typeof fontSizeStyle;
+export type BorderRadiusStyle = keyof typeof borderRadiusStyle;
+export type Height = keyof typeof height;
+export type GapStyle = keyof typeof gapStyle;
+
+export type Size = "xs" | "sm" | "md" | "lg" | "xlg";
