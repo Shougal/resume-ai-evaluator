0fd9b273a2a08c688fbdf08622a4fb8195e22f9b
shougal <xdq9qa@virginia.edu>
2025-07-21 11:42:40 +0300

    chore(Button): created button component with animated motion

---
+++ frontend/src/components/Button/index.tsx
@@ -0,0 +1,30 @@
+import type { ButtonProps, ButtonStyleProps } from "./types";
+import { getVariantStyle } from "./styles";
+import { motion } from "motion/react";
+
+export const Button = ({
+  size = "md",
+  variant,
+  disabled,
+  onClick,
+  children,
+  accessibilityLabel,
+  ...rest
+}: ButtonProps) => {
+  const styleProps: ButtonStyleProps = { variant, size, disabled };
+  const style = getVariantStyle(styleProps);
+
+  return (
+    <motion.button
+      whileHover={disabled ? undefined : { scale: 1.1 }}
+      transition={{ duration: 0.3, ease: "easeOut" }}
+      onClick={onClick}
+      aria-label={accessibilityLabel}
+      style={style}
+      disabled={disabled}
+      {...rest}
+    >
+      {children}
+    </motion.button>
+  );
+};
