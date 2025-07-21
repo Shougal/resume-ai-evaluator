ca43ac5deb89ff56704567dabd250f67b1e2ab06
shougal <xdq9qa@virginia.edu>
2025-07-21 11:41:21 +0300

    chore(button Type): created button types

---
+++ frontend/src/components/Button/types.ts
@@ -0,0 +1,17 @@
+import type { Size } from "../../theme/types";
+
+export type Variant = "primary";
+
+// primary sec ter
+
+export interface ButtonStyleProps {
+  variant: Variant;
+  size: Size;
+  disabled?: boolean;
+}
+//TODO Enhancement:on Mouse leave, up, and down
+export interface ButtonProps extends ButtonStyleProps {
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  children: React.ReactNode;
+  accessibilityLabel?: string;
+}
