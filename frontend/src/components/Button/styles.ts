e4a0ef123813e26117bf087e1b34d4433e00439a
shougal <xdq9qa@virginia.edu>
2025-07-21 11:42:57 +0300

    style(Button): created button style

---
+++ frontend/src/components/Button/styles.ts
@@ -0,0 +1,33 @@
+import type { ButtonStyleProps } from "./types";
+import {
+  paddingStyle,
+  fontSizeStyle,
+  borderRadiusStyle,
+  height,
+  buttonBackground,
+} from "../../theme/layout";
+
+const baseStyle = (props: ButtonStyleProps) => {
+  return {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: paddingStyle[props.size],
+    fontSize: fontSizeStyle[props.size],
+    borderRadius: borderRadiusStyle[props.size],
+    height: height.buttonHeight,
+    background: buttonBackground.primary,
+  };
+};
+
+// Main function to export
+export const getVariantStyle = (
+  props: ButtonStyleProps
+): React.CSSProperties => {
+  switch (props.variant) {
+    case "primary":
+      return baseStyle(props);
+    default:
+      return baseStyle(props); // fallback
+  }
+};
