350b3eed21739db617e40a29f0ea23cea42301fb
shougal <xdq9qa@virginia.edu>
2025-07-21 11:44:26 +0300

    test(Button): created TDD testing for button.

---
+++ frontend/src/components/Button/__tests__/Button.test.tsx
@@ -0,0 +1,38 @@
+import { describe, it, expect, vi } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Button } from "..";
+
+describe("Button should be rendered", () => {
+  it("renders the button component", () => {
+    render(
+      <Button
+        size="lg"
+        variant="primary"
+        onClick={() => console.log("clicked")}
+      >
+        hey there render
+      </Button>
+    );
+    screen.debug();
+  });
+});
+
+describe("form must be submitted upon button click of type submit", () => {
+  it("renders form", () => {
+    const handleSubmit = vi.fn((e) => {
+      e.preventDefault();
+      console.log("submitted form");
+    });
+    const { getByRole } = render(
+      <form onSubmit={handleSubmit}>
+        <Button size="sm" variant="primary" onClick={handleSubmit}>
+          {" "}
+          hey there form
+        </Button>
+      </form>
+    );
+    const button = getByRole("button");
+    fireEvent.click(button);
+    expect(handleSubmit).toHaveBeenCalled();
+  });
+});
