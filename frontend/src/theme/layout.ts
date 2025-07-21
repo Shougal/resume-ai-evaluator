ba78bd23e92413bb8e87ee524e458eb7576528f6
shougal <xdq9qa@virginia.edu>
2025-07-21 13:30:16 +0300

    chore (layout): added global themes

--- frontend/src/theme/layout.ts
+++ frontend/src/theme/layout.ts
@@ -9,13 +9,14 @@ export const paddingStyle = {
 export const fontSizeStyle = {
   xs: "12px",
   sm: "14px",
-  md: "16px",
-  lg: "18px",
-  xlg: "20px",
+  md: "20px",
+  lg: "30px",
+  xlg: "50px",
+  title: "81px",
 };
 
 export const borderRadiusStyle = {
-  xs: "5px",
+  xs: "9px",
   sm: "9px",
   md: "13px",
   lg: "17px",
@@ -34,6 +35,20 @@ export const gapStyle = {
   xlg: "40px",
 };
 
-export const buttonBackground = {
+export const Background = {
   primary: "linear-gradient(0.6turn, #763AF5,40%,  #A604F2 )",
+  text: "linear-gradient(to right, #FFFFFF 0%, #763AF5 48%, #A604F2 99%)",
+};
+
+export const fontFamily = {
+  default: "Inter, sans-serif",
+};
+
+export const fontWeight = {
+  xlg: 800,
+  lg: 600,
+  md: 500,
+  sm: 400,
+  xs: 300,
+  xxs: 200,
 };
