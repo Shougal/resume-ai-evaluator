845cff653ec931c23249f7b00e9f411d30b4820c
shougal <xdq9qa@virginia.edu>
2025-07-22 11:02:40 +0300

    Chore(Section): created section component with flex items and optional display

--- frontend/src/components/Section/index.tsx
+++ frontend/src/components/Section/index.tsx
@@ -1,7 +1,39 @@
-// import type { SectionProps } from "./types";
+import React from "react";
+import { isValidElement } from "react";
 
-// export const Section =({title, children, ...rest}: SectionProps) =>{
-//     return(
+import type { SectionProps } from "./types";
+import { sectionStyle } from "./styles";
+import { Text } from "../Text";
 
-//     )
-// }
+export const Section = ({
+  title,
+  children,
+  displayDirection,
+}: SectionProps) => {
+  const style = sectionStyle({
+    displayDirection,
+  });
+
+  return (
+    <div>
+      {title && <Text {...title}>{title.children}</Text>}
+      {/* flex box with items */}
+      <div style={style}>
+        {React.Children.map(children, (child) =>
+          isValidElement(child) ? (
+            <div
+              style={{
+                flex: 1,
+                flexShrink: 1,
+              }}
+            >
+              {child}
+            </div>
+          ) : (
+            child
+          )
+        )}
+      </div>
+    </div>
+  );
+};
