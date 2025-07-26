539cb29c3c8a99172b41751fd89dd36b788e41ab
shougal <xdq9qa@virginia.edu>
2025-07-26 20:14:56 +0300

    feat: add /resume route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import multer from "multer";
+import { resumeRouter } from "./routes/resumeRoutes";
 
+const upload = multer({ dest: "uploads/" });
 dotenv.config(); //loads env variables
 
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 
 app.use(cors()); // allows cross-origin requests
@@ -13,6 +17,9 @@ app.use(express.json()); // parses incoming JSON request bodies
 app.get("/", (req, res) => {
   res.send("Hello from Resume AI backend!");
 });
+
+//Route to resume evaluations:
+app.use("/resume", resumeRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
